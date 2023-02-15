13bf37d675b84d1cdf96c44e42e0df16c82052a1
wdk59 <dookong59@gmail.com>
2023-02-16 04:25:43 +0900

    순서 돌아가기 적용
    1. 플레이어 순서대로 순서가 돌아감
    2. 윷이나 모가 나오면 한 번 더 던지기
    3. 윷 결과 배열에 저장
    4. 윷 던지고 나서 움직일 말을 선택하면 움직일 준비
    
    남은 거
    1. 윷 결과에 따라 갈 수 있는 곳 계산해서 표시해주기
    2. 진짜로 말 움직이기
    3. 업기
    4. 잡기
    5. 승패 판정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    int[] yutSeat = new int[29];    // 자리별로 위치한 플레이어 구분. 인덱스 순서는 윷 자리에 대한 게임 오브젝트 배치 순서와 동일.
+    private PlayerMove player;      // 플레이어의 차례가 될 때마다 각 차례의 플레이어로 오브젝트 변경
+
+    public GameObject clickedObject = null;    // 클릭 이벤트 발생 시 선택되는 게임 오브젝트 저장
+
+    int[] yutSeat = new int[29];    // 자리별로 위치한 플레이어 구분. 인덱스 순서는 윷 자리에 대한 게임 오브젝트 배치 순서와 동일
     bool[] participant = new bool[] { false, false, false, false };     // 참여자 (최대 4명)
+    public int participantTop = -1;     // participant 배열의 마지막 인덱스
+
+    public int playerTurn = 0;          // 어느 플레이어 차례인지 저장해두는 변수
+    public bool turnChanged = false;    // 차례가 지금 막 바뀌었는지 알려주는 변수
+
     short[] yuts = new short[] { 0, 0, 0, 0 };  // 0: 대기, 1: 둥근 면, 2: 평평한 면, 3: 낙
     short[] yutCount = new short[] { 0, 0, 0 };    // 0번 인덱스: 둥근 면의 개수, 1번 인덱스: 평평한 면의 개수, 2번 인덱스: 낙인 윷의 개수
 
@@ -14,32 +23,98 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        participant[0] = true;  // user
-        participant[1] = true;  // computer
+        /* initializing: 변수 초기화 */
+        clickedObject = null;
+        participantTop = -1;
+        playerTurn = 0;
+        turnChanged = false;
+        result = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            participant[i] = false;
+            yuts[i] = 0;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            yutCount[i] = 0;
+        }
+
+        /* 게임 준비 */
+        participant[++participantTop] = true;  // user
+        participant[++participantTop] = true;  // computer
         GameObject.Find("UI").transform.Find("Yut_Result_Panel").gameObject.SetActive(false);
 
         for (int i = 0; i < 4; i++)
         {
             if (participant[i] == true) {
-                GameObject.Find("UI").transform.Find("Player_Panel").transform.GetChild(i).gameObject.SetActive(true);
+                GameObject.Find("UI").transform.GetChild(i).gameObject.SetActive(true);
             }
             else {
-                GameObject.Find("UI").transform.Find("Player_Panel").transform.GetChild(i).gameObject.SetActive(false);
+                GameObject.Find("UI").transform.Find("Player_Panel_Group").transform.GetChild(i).gameObject.SetActive(false);
+                GameObject.Find("Player_Group").transform.GetChild(i).gameObject.SetActive(false);
             }
-
         }
 
-        ThrowYuts();
+        // 플레이어1에게  차례를 넘겨줌
+        playerTurn = 1;
+        turnChanged = true;
+        GameObject.Find("UI").transform.Find("Player_Panel_Group").transform.GetChild(playerTurn - 1).gameObject.GetComponent<SpriteRenderer>().material.color = Color.gray;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 마우스 클릭 시 실행
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 오브젝트를 취득해서 변수에 넣음
+            clickedObject = GetClickedObject();
+            // 말 선택 단계이고, 아직 말을 선택 안 했는지 확인
+            // 선택된 오브젝트의 부모 오브젝트의 이름과 현재 차례의 플레이어의 말 그룹 오브젝트의 이름이 같은지 확인
+            if (player.selectingPawn && !player.pawnSelected && clickedObject.transform.parent.name == GameObject.Find("Player_Group").transform.GetChild(playerTurn - 1).name)
+            {
+                player.pawnSelected = true;
+            }
+            //Debug.Log(clickedObject);
+        }
+
+        // 차례 넘어갔을 때 실행
+        if (turnChanged)
+        {
+            Debug.Log("gmUpdate: 차례 바꼈다!");
+            turnChanged = false;
+            player = GameObject.Find("Player_Group").transform.GetChild(playerTurn - 1).GetComponent<PlayerMove>();
+            player.myTurn = true;
+            Debug.Log("gmUpdate 차례: " + GameObject.Find("Player_Group").transform.GetChild(playerTurn - 1));
+
+            // 윷을 던져서 결과를 플레이어 클래스의 배열에 저장해준다.
+            ThrowYuts();
+        }
+    }
+
+    // 클릭된 오브젝트 받아오기: 마우스 클릭 시 실행
+    GameObject GetClickedObject()
+    {
+        GameObject target = null;
+
+        // 마우스 위치 취득
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // 마우스 위치로 레이 던짐
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+        // 던진 레이가 히트되었다면 실행
+        if (hit.collider != null)
+        {
+            // 오브젝트를 취득해서 변수에 넣음
+            target = hit.collider.transform.gameObject;
+            Debug.Log(target);
+        }
+
+        return target;
     }
 
     // throw yuts: 윷 값을 랜덤으로 받고 결과 나타내기
-    void ThrowYuts()
+    public void ThrowYuts()
     {
         GameObject.Find("UI").transform.Find("Yut_Result_Panel").gameObject.SetActive(true);
 
@@ -54,13 +129,13 @@ public class GameManager : MonoBehaviour
             // 윷 값에 따라 개수 세기
             switch (yuts[i])
             {
-                case 1:
+                case 1:             // 둥근 면
                     yutCount[0]++;
                     break;
-                case 2:
+                case 2:             // 평평한 면
                     yutCount[1]++;
                     break;
-                case 3:
+                case 3:             // 낙
                     yutCount[2]++;
                     break;
                 default:
@@ -87,11 +162,18 @@ public class GameManager : MonoBehaviour
             } else if (yutCount[0] == 3)    // 걸: 둥근 면 3, 평평한 면 1
             {
                 result = 3;
-            } else if (yutCount[0] == 4)    // 도: 둥근 면 4, 평평한 면 0
+            } else if (yutCount[0] == 4)    // 모: 둥근 면 4, 평평한 면 0
             {
                 result = 5;
             }
         }
+
+        // 플레이어에게 결과 전달
+        player.waitYut[++player.waitYutTop] = result;
+        player.remainingYut += player.waitYut[player.waitYutTop];
+        Debug.Log("gm_ThrowYuts: player.waitYut List");
+        for (int i = 0; i <= player.waitYutTop; i++)
+            Debug.Log(player.waitYut[i]);
 
         // 던지는 모습 표현 및 시간 지연 (2초)
         StartCoroutine(ThrowingYutTimer());
@@ -136,5 +218,14 @@ public class GameManager : MonoBehaviour
         yuts = new short[] { 0, 0, 0, 0 };
         yutCount = new short[] { 0, 0, 0 };
         result = 0;
+
+        // 결과가 윷이나 모일 때는 윷을 다시 던지고, 아니면 윷 던지기 끝내기
+        if (player.waitYut[player.waitYutTop] == 4 || player.waitYut[player.waitYutTop] == 5)
+            ThrowYuts();
+        else
+        {
+            clickedObject = null;
+            player.selectingPawn = true;
+        }
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,23 +4,64 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    bool canMove = false;   // 차례가 되면 윷을 던진 후에 움직이도록 제어
+    public GameManager gameManager;
+
+    public bool myTurn = false;         // 내 차례인지 판별하는 변수
+    public bool selectingPawn = false;  // 내 차례일 때 움직일 말을 선택하는 단계인지 판별하는 변수
+    public bool pawnSelected = false;          // 움직일 말을 선택했는지 판별하는 변수
+
     int[] movingVector = new int[] { 0, 0 };    // 말이 움직일 방향과 크기를 가짐
-    int[] waitYut = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // 1: 도, 2: 개, 3: 걸, 4: 윷, 5: 모, 6: 빽도, 7: 낙
+
+    public int[] waitYut = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // 1: 도, 2: 개, 3: 걸, 4: 윷, 5: 모, 6: 빽도, 7: 낙
+    public int waitYutTop = -1;             // waitYut의 마지막 인덱스를 가리킴 (-1이면 아무것도 없음)
+    public int remainingYut = 0;            // waitYut에 아무것도 없는지 확인. waitYut의 값이 바뀔 때마다 업데이트.
 
     // Start is called before the first frame update
     void Start()
     {
-        canMove = false;
-        movingVector[0] = 0;
-        movingVector[1] = 0;
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 내 차례에 윷을 다 던졌고, 대기열(waitYut)에 남은 이동 횟수가 없을 때까지 움직일 수 있음 (GM의 HidingYutResult 함수에서 selectingPawn이 true로 바뀜)
+        if (selectingPawn && !pawnSelected && remainingYut > 0) // 아직 움직일 말 선택 안 함
+        {
+            Debug.Log("pUpdate: 움직여!");
+            // 타이머 걸고 시간 끝날 때까지 선택 안 하면 랜덤으로 움직이면 될듯
+        }
+        else if (selectingPawn && pawnSelected && remainingYut > 0) // 움직일 말 선택 함: 이제 목적지 선택하고 움직여야 함
+        {
+            // 움직일 목적지 선택: 이동 가능한 목적지 표시해주기, 선택하면 waitYut에서 해당 목적지 지워주고 자리 당기기, waitYutTop 업데이트, remainingYut 업데이트
+
+            // 선택된 말을 목적지까지 이동
+            gameManager.clickedObject.transform.position = new Vector2(4f, -4f);
+
+            // 현재 내 상태 초기화
+            Initialize();
+            // 순서 넘기기
+            GameObject.Find("UI").transform.Find("Player_Panel_Group").transform.GetChild(gameManager.playerTurn - 1).gameObject.GetComponent<SpriteRenderer>().material.color = Color.white;
+            if (gameManager.playerTurn < gameManager.participantTop + 1)
+                gameManager.playerTurn++;
+            else
+                gameManager.playerTurn = 1;
+            GameObject.Find("UI").transform.Find("Player_Panel_Group").transform.GetChild(gameManager.playerTurn - 1).gameObject.GetComponent<SpriteRenderer>().material.color = Color.gray;
+            gameManager.turnChanged = true;
+            Debug.Log("다음 순서: " + gameManager.playerTurn);
+        }
     }
 
-    
+    void Initialize()
+    {
+        myTurn = false;
+        selectingPawn = false;
+        pawnSelected = false;
+        movingVector[0] = 0;
+        movingVector[1] = 0;
+        for (int i = 0; i < 10; i++)
+            waitYut[i] = 0;
+        waitYutTop = -1;
+        remainingYut = 0;
+    }
 }
